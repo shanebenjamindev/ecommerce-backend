337534a39eedf68462e282d217dd264c2586f489
Your Name <you@example.com>
2024-03-07 21:31:35 +0700

    added env in sever

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.use(cors())
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+// app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
 app.use(bodyParser.json())
 app.use(cookieParser())
 
@@ -21,7 +21,7 @@ routes(app);
 
 mongoose.connect(`${process.env.MONGO_DB}`)
     .then(() => {
-        // console.log('Connect Db success!')
+        console.log('Connect Db success!')
     })
     .catch((err) => {
         // console.log(err)
